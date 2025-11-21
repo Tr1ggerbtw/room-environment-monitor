ab5391f061dd2b0d65e2f8ddc92343c86b9e837f
Tr1ggerbtw <lehesadanylo27@gmail.com>
2025-11-21 12:46:37 +0200

    feat: Add initial esp32 project structure

---
+++ firmware/esp32_sensor_actuator/main/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_wifi.h"
+#include "esp_event_loop.h"
+
+esp_wifi_set_mode();
+void app_main(void)
+{
+    printf("Hello world!");
+}
