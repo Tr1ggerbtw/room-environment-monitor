8b3eb56a2236b5bd3f03db1dcf98b1b1aba70af2
Tr1ggerbtw <lehesadanylo27@gmail.com>
2025-11-22 19:15:21 +0200

    feat: Complete full Wi-Fi STA connection flow

--- firmware/esp32_sensor_actuator/main/main.c
+++ firmware/esp32_sensor_actuator/main/main.c
@@ -40,12 +40,68 @@ static void event_handler(void* arg, esp_event_base_t event_base, int32_t event_
           }
 }
          
-// void wifi_init_sta()
-// {
+void wifi_init_sta()
+{
+s_wifi_event_group = xEventGroupCreate();
 
-// }
+    ESP_ERROR_CHECK(esp_netif_init());
+
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
+
+    wifi_init_config_t wifiInitCfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&wifiInitCfg));
+
+    esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instance_got_ip;
+
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_any_id));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+                                                        IP_EVENT_STA_GOT_IP,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_got_ip));
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = MY_SSID,
+            .password = MY_PASS,
+            .threshold.authmode = WIFI_AUTH_WPA_WPA2_PSK
+        }
+    };
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+            WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+            pdFALSE,
+            pdFALSE,
+            portMAX_DELAY);
+
+    if(bits & WIFI_CONNECTED_BIT)
+    {
+        ESP_LOGI(TAG, "Connected to ap");
+    }   else if (bits & WIFI_FAIL_BIT) 
+        {
+            ESP_LOGI(TAG, "Failed to connect to AP");
+        } else {
+            ESP_LOGI(TAG, "Unexpected event");
+        }
+}
 
 void app_main(void) {
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
 
-    // wifi_init_sta();
+    wifi_init_sta();
 }
