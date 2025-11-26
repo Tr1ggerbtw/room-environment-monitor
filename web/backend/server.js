c0a805eb3017ae0e3164c3a5eadd55ef9428bda6
Tr1ggerbtw <lehesadanylo27@gmail.com>
2025-11-27 00:52:33 +0200

    feat(backend): process command from backend with express, and then via axios send 'command' to firmware

--- web/backend/server.js
+++ web/backend/server.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const app = express()
+const axios = require('axios');
 const port = 3000
+const FIRMWARE_URL = "http://192.168.0.100:80/fan"
 
 app.use(express.json());
-
+app.use(express.text());
 temperature = 0;
 humidity = 0; 
+
+async function sendToFirmware(command)
+{
+    axios.post(FIRMWARE_URL, command, {
+        headers: {
+            'Content-Type': 'text/plain'
+        }
+    })
+}
 
 app.post('/ping', (req, res) => {
 
@@ -13,7 +24,12 @@ app.post('/ping', (req, res) => {
     humidity = req.body.humidity;
 
     console.log("Temperature: %s\n Humidity: %s\n", temperature, humidity);
-    res.send( { status: "ok" })   
+    res.send( { status: "ok" }); 
+})
+
+app.post('/backend', (req,res) => {
+    sendToFirmware(req.body);
+    res.send( {status: "ok"});
 })
 
 app.listen(port, () => {
